47ef905f0d429d5283e1812c9ce6600bba46aa1d
kruulik <karoliskrulis@gmail.com>
2017-12-06 14:27:20 -0500

    Fixing webpack for v3 (its a mess)

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -5,7 +5,7 @@ var webpack = require('webpack');
 
 var assetsPath = path.resolve(__dirname, '../static/dist');
 var host = (process.env.HOST || 'localhost');
-var port = (+process.env.PORT + 1) || 3001;
+var port = (+ process.env.PORT + 1) || 3001;
 
 // https://github.com/halt-hammerzeit/webpack-isomorphic-tools
 var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
@@ -20,7 +20,6 @@ try {
   console.error('==>     ERROR: Error parsing your .babelrc.');
   console.error(err);
 }
-
 
 var babelrcObjectDevelopment = babelrcObject.env && babelrcObject.env.development || {};
 
@@ -45,7 +44,11 @@ for (var i = 0; i < babelLoaderQuery.plugins.length; ++i) {
 }
 
 if (!reactTransform) {
-  reactTransform = ['react-transform', {transforms: []}];
+  reactTransform = [
+    'react-transform', {
+      transforms: []
+    }
+  ];
   babelLoaderQuery.plugins.push(reactTransform);
 }
 
@@ -54,11 +57,7 @@ if (!reactTransform[1] || !reactTransform[1].transforms) {
 }
 
 // make sure react-transform-hmr is enabled
-reactTransform[1].transforms.push({
-  transform: 'react-transform-hmr',
-  imports: ['react'],
-  locals: ['module']
-});
+reactTransform[1].transforms.push({transform: 'react-transform-hmr', imports: ['react'], locals: ['module']});
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -78,36 +77,119 @@ module.exports = {
     publicPath: 'http://' + host + ':' + port + '/dist/'
   },
   module: {
-    loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel?' + JSON.stringify(babelLoaderQuery), 'eslint-loader']},
-      { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.less$/, loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!postcss-loader!less?outputStyle=expanded&sourceMap' },
-      { test: /\.scss$/, loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!postcss-loader!sass?outputStyle=expanded&sourceMap' },
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
-      { test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240' }
+    rules: [
+      {
+        test: /\.jsx$/,
+        use: [
+            {
+                loader: 'babel-loader'
+            }
+        ],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+            {
+                loader: 'style-loader'
+            },
+            {
+                loader: 'css-loader',
+                options: {
+                    sourceMap: true,
+                    importLoaders: 1,
+                    minimize: true
+                }
+            },
+            {
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true
+                }
+            }
+        ],
+      }, {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            use: 'url-loader',
+
+            options: {
+              limit: 10000,
+              mimetype: 'application/font-woff'
+            }
+          }
+        ]
+      }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            use: 'url-loader',
+
+            options: {
+              limit: 10000,
+              mimetype: 'application/font-woff'
+            }
+          }
+        ]
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            use: 'url-loader',
+
+            options: {
+              limit: 10000,
+              mimetype: 'application/octet-stream'
+            }
+          }
+        ]
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            use: 'file-loader'
+          }
+        ]
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            use: 'url-loader',
+
+            options: {
+              limit: 10000,
+              mimetype: 'image/svg+xml'
+            }
+          }
+        ]
+      }, {
+        test: webpackIsomorphicToolsPlugin.regular_expression('images'),
+        use: [
+          {
+            use: 'url-loader',
+
+            options: {
+              limit: 10240
+            }
+          }
+        ]
+      }
     ]
   },
-  progress: true,
+  // progress: true,
   resolve: {
-    modulesDirectories: [
-      'src',
-      'node_modules'
+    modules: [
+      path.join(__dirname, "src"),
+      "node_modules"
     ],
-    extensions: ['', '.json', '.js', '.jsx']
+    extensions: ['.json', '.js', '.jsx']
   },
   plugins: [
     // hot reload
     new webpack.HotModuleReplacementPlugin(),
     new webpack.IgnorePlugin(/webpack-stats\.json$/),
-    new webpack.DefinePlugin({
-      __CLIENT__: true,
-      __SERVER__: false,
-      __DEVELOPMENT__: true
-    }),
+    new webpack.DefinePlugin({__CLIENT__: true, __SERVER__: false, __DEVELOPMENT__: true}),
+    new webpack.LoaderOptionsPlugin({minimize: true}),
     webpackIsomorphicToolsPlugin.development()
   ]
 };
