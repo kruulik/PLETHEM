346923932adb8ee4043e3dd732d41cb0365b973e
kruulik <karoliskrulis@gmail.com>
2017-12-14 14:59:13 -0500

    Converted existing structure to mimic JAVA app

--- src/actions/settingsActions.js
+++ src/actions/settingsActions.js
@@ -1,46 +1,35 @@
 
-// Dataset
-export const CHANGE_DATASET = 'CHANGE_DATASET';
-
-// Physiology
-export const CHANGE_SPECIES = 'CHANGE_SPECIES';
-export const CHANGE_AGEDEP = 'CHANGE_AGEDEP';
+// Analysis
+export const CHANGE_ANALYSIS = 'CHANGE_ANALYSIS';
 export const CHANGE_VARIABILITY = 'CHANGE_VARIABILITY';
 export const CHANGE_POPSIZE = 'CHANGE_POPSIZE';
-export const CHANGE_PERCMALE = 'CHANGE_PERCMALE';
-export const CHANGE_MINAGE = 'CHANGE_MINAGE';
-export const CHANGE_MAXAGE = 'CHANGE_MAXAGE';
 
-// ADME
-export const CHANGE_METPS = 'CHANGE_METPS';
-export const CHANGE_IVASSAY = 'CHANGE_IVASSAY';
-export const CHANGE_SATMET = 'CHANGE_SATMET';
-export const CHANGE_RENALELIM = 'CHANGE_RENALELIM';
-export const CHANGE_EHCCB = 'CHANGE_EHCCB';
+// Model
+export const CHANGE_PCMETHOD = 'CHANGE_PCMETHOD';
+export const CHANGE_ORALABS = 'CHANGE_ORALABS';
+export const CHANGE_EHPMET = 'CHANGE_EHPMET';
+export const CHANGE_METTRACK = 'CHANGE_METTRACK';
+export const CHANGE_ACTIVETRANS = 'CHANGE_ACTIVETRANS';
+export const CHANGE_INHIB = 'CHANGE_INHIB';
+
+// Units
+export const CHANGE_MASSUNIT = 'CHANGE_MASSUNIT';
+export const CHANGE_VOLUNIT = 'CHANGE_VOLUNIT';
+export const CHANGE_TIMEUNIT = 'CHANGE_TIMEUNIT';
 
 // Simulation
 export const CHANGE_TIMESTEP = 'CHANGE_TIMESTEP';
-export const CHANGE_STARTAGE = 'CHANGE_STARTAGE';
-export const CHANGE_DURATION = 'CHANGE_DURATION';
+export const CHANGE_ALGORITHM = 'CHANGE_ALGORITHM';
+export const CHANGE_RELERR = 'CHANGE_RELERR';
+export const CHANGE_ABSERR = 'CHANGE_ABSERR';
 
 
-// Dataset
-export const setDataset = value => {
+// Analysis
+export const setAnalysisType = value => {
   return{
-    type: CHANGE_DATASET,
+    type: CHANGE_ANALYSIS,
     value
 }}
-
-// Physiology
-export const setSpecies = value => ({
-    type: CHANGE_SPECIES,
-    value
-})
-
-export const setAgeDep = value => ({
-  type: CHANGE_AGEDEP,
-  value
-})
 
 export const setVariability = value => ({
     type: CHANGE_VARIABILITY,
@@ -48,63 +37,77 @@ export const setVariability = value => ({
 })
 
 export const setPopSize = value => ({
-    type: CHANGE_POPSIZE,
+  type: CHANGE_POPSIZE,
+  value
+})
+
+
+// Model
+export const setPCMethod = value => ({
+  type: CHANGE_PCMETHOD,
+  value
+})
+
+export const setOralAbsMethod = value => ({
+  type: CHANGE_ORALABS,
+  value
+})
+
+export const setEHPMet = value => ({
+    type: CHANGE_EHPMET,
     value
 })
 
-export const setPercMale = value => ({
-    type: CHANGE_PERCMALE,
+export const setMetTracking = value => ({
+  type: CHANGE_METTRACK,
+  value
+})
+
+export const setActiveTransport = value => ({
+  type: CHANGE_ACTIVETRANS,
+  value
+})
+
+export const setInhib = value => ({
+  type: CHANGE_INHIB,
+  value
+})
+
+
+// Units
+export const setMassUnit = value => ({
+    type: CHANGE_MASSUNIT,
     value
 })
 
-export const setMinAge = value => ({
-    type: CHANGE_MINAGE,
+export const setVolUnit = value => ({
+    type: CHANGE_VOLUNIT,
     value
 })
 
-export const setMaxAge = value => ({
-    type: CHANGE_MAXAGE,
+export const setTimeUnit = value => ({
+    type: CHANGE_TIMEUNIT,
     value
 })
 
-// ADME
-export const setMetParaSource = value => ({
-    type: CHANGE_METPS,
-    value
-})
-
-export const setIVAssay = value => ({
-    type: CHANGE_IVASSAY,
-    value
-})
-
-export const setSatMet = value => ({
-    type: CHANGE_SATMET,
-    value
-})
-
-export const setRenalElim = value => ({
-    type: CHANGE_RENALELIM,
-    value
-})
-
-export const setIncludeEhccb = value => ({
-    type: CHANGE_EHCCB,
-    value
-})
 
 // Simulation
 export const setTimestep = value => ({
-    type: CHANGE_TIMESTEP,
-    value
+  type: CHANGE_TIMESTEP,
+  value
 })
 
-export const setStartAge = value => ({
-    type: CHANGE_STARTAGE,
-    value
+export const setAlgorithm = value => ({
+  type: CHANGE_ALGORITHM,
+  value
 })
 
-export const setDuration = value => ({
-    type: CHANGE_DURATION,
-    value
+export const setRelErr = value => ({
+  type: CHANGE_RELERR,
+  value
+})
+
+export const setAbsErr = value => ({
+  type: CHANGE_ABSERR,
+  value
 })
