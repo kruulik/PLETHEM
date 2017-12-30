fee55447c06108ce66f6d3a3d737ba6bdab9092a
kruulik <karoliskrulis@gmail.com>
2017-12-30 18:34:01 -0500

    Fixing deleted folder

--- inst/webpack/common-paths.js
+++ inst/webpack/common-paths.js
@@ -4,6 +4,5 @@ module.exports = {
     outputPath: resolve(__dirname, "../", "www"),
     contentBasePath: resolve(__dirname, "../", "public" ),
     srcPath: resolve(__dirname, "../src"),
-    // stylesheetsPath: resolve(__dirname, "../stylesheets"),
     bundleVisualizerStatsPath: "../dist/stats" // we can't use resolve here because the plugin fails
 };
